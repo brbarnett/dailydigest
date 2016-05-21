c44baf3ae5a40a3937ee73edf6a70ab7882ce3e5
brbarnett <barnett@echosolutionsgroup.com>
2016-05-20 19:24:10 -0700

    Initial commit

---
+++ job.js
@@ -0,0 +1,51 @@
+const request = require('request');
+const _ = require('lodash');
+
+const teamsOfInterest = ['CHC', 'SF'];
+
+const url = getFormattedUrl();
+
+request.get({
+    url: url,
+    json: true
+}, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        const games = body.data.games.game;
+        const gamesOfInterest = _.filter(games, (x) => _.includes(teamsOfInterest, x.home_name_abbrev));
+        const formattedGames = getFormattedGames(gamesOfInterest);
+
+        console.log(gamesOfInterest);
+        console.log(formattedGames);
+    }
+});
+
+function getFormattedUrl() {
+    const now = new Date();
+
+    const date = {
+        year: now.getFullYear(),
+        month: _.padStart(now.getMonth() + 1, 2, '0'),
+        day: _.padStart(now.getDate() + 1, 2, '0')
+    };
+
+    const url = `http://m.mlb.com/gdcross/components/game/mlb/year_${date.year}/month_${date.month}/day_${date.day}/master_scoreboard.json`;
+
+    return url;
+}
+
+function getFormattedGames(games) {
+    const formattedGames = _.map(games, (game) => {
+        return {
+            away: {
+                name: game.away_team_name
+            },
+            home: {
+                name: game.home_team_name
+            },
+            location: game.location,
+            time: `${game.home_time} ${game.home_ampm} ${game.home_time_zone}`
+        };
+    });
+
+    return formattedGames;
+}
